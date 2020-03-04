6bf15b7db0dd0849cf5a5a7adba56193f9b4f8da
mingyang96ong <56052262+mingyang96ong@users.noreply.github.com>
2020-03-04 17:03:30 +0800

    Last uploaded on 04/03/2020 17:02

--- src/sensor/SimulatorSensor.java
+++ src/sensor/SimulatorSensor.java
@@ -27,25 +27,25 @@ public class SimulatorSensor extends Sensor{
 		// "North", "East", "South", "West"
 		for (int i = 1; i <= Constant.SHORTSENSORMAXRANGE; i ++ ) {
 			if (sensorValue[0] == null && trueMap.getGrid(x+sensorLocation[0][0] + i * sensorDirection[0][0], y+sensorLocation[0][1] + i * sensorDirection[0][1]).compareTo(Constant.POSSIBLEGRIDLABELS[2]) == 0) {
-				sensorValue[0] = getSensorValue(i, "SHORT");
+				sensorValue[0] = getSensorValue(0, i, "SHORT");
 			}
 			if (sensorValue[1] == null && trueMap.getGrid(x+sensorLocation[1][0] + i * sensorDirection[0][0], y+sensorLocation[1][1] + i * sensorDirection[0][1]).compareTo(Constant.POSSIBLEGRIDLABELS[2]) == 0) {
-				sensorValue[1] = getSensorValue(i, "SHORT");
+				sensorValue[1] = getSensorValue(1, i, "SHORT");
 			}
 			if (sensorValue[2] == null && trueMap.getGrid(x+sensorLocation[2][0] + i * sensorDirection[0][0], y+sensorLocation[2][1] + i * sensorDirection[0][1]).compareTo(Constant.POSSIBLEGRIDLABELS[2]) == 0){
-				sensorValue[2] = getSensorValue(i, "SHORT");
+				sensorValue[2] = getSensorValue(2, i, "SHORT");
 			}
 			if (sensorValue[3] == null && trueMap.getGrid(x+sensorLocation[3][0] + i * sensorDirection[1][0], y+sensorLocation[3][1] + i * sensorDirection[1][1]).compareTo(Constant.POSSIBLEGRIDLABELS[2]) == 0) {
-				sensorValue[3] = getSensorValue(i, "SHORT");
+				sensorValue[3] = getSensorValue(3, i, "SHORT");
 			}
 			if (sensorValue[4] == null && trueMap.getGrid(x+sensorLocation[4][0] + i * sensorDirection[1][0], y+sensorLocation[4][1] + i * sensorDirection[1][1]).compareTo(Constant.POSSIBLEGRIDLABELS[2]) == 0) {
-				sensorValue[4] = getSensorValue(i, "SHORT");
+				sensorValue[4] = getSensorValue(4, i, "SHORT");
 			}
 		}
 		
 		for (int i = 1; i <= Constant.FARSENSORMAXRANGE; i ++ ) {
 			if (sensorValue[5] == null && trueMap.getGrid(x+sensorLocation[5][0] + i * sensorDirection[2][0], y+sensorLocation[5][1] + i * sensorDirection[2][1]).compareTo(Constant.POSSIBLEGRIDLABELS[2]) == 0) {
-				sensorValue[5] = getSensorValue(i, "FAR");
+				sensorValue[5] = getSensorValue(5, i, "FAR");
 			}
 		}
 		
@@ -53,16 +53,8 @@ public class SimulatorSensor extends Sensor{
 		this.sensorValue = String.join(" ", sensorValue);
 	}
 	
-	private String getSensorValue(int i, String mode) {
-		if (mode.toUpperCase().compareTo("SHORT") == 0) {
-			return "" + ((i-1) * 10 + Constant.SHORTSENSOROFFSET) + ".0";
-		}
-		else if (mode.toUpperCase().compareTo("FAR") == 0) {
-			return "" + ((i-1) * 10 + Constant.FARSENSOROFFSET) + ".0";
-		}
-		else {
-			return "";
-		}
+	private String getSensorValue(int s, int i, String mode) {
+		return "" + (Constant.SENSOR_RANGES[s][i-1]-1);
 	}
 	
 	public String[] getAllSensorsValue(int x, int y, int direction) {
